8cebf42a7ae5a023203628b0077b40408f7f1441
ODA terutaka <t.oda@nttceng.com>
2023-07-24 12:06:04 +0900

    add api

--- api/message/index.js
+++ api/message/index.js
@@ -6,8 +6,6 @@ module.exports = async function (context, req) {
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    //context.res.json({text: responseMessage});
+  
 }
